37d3e4a20ebb31b69461500a519d15c97538bbb2
sincereman <as@vellit.ru>
2023-10-29 23:21:56 +0300

    Lesson25-Backup

---
+++ lesson25-Backup/vm/v2a.py
@@ -0,0 +1,12 @@
+from vagranttoansible import vagranttoansible
+
+# pip install vagranttoansible
+# python3 v2a.py -o inventories/hosts
+vagranttoansible.DEFAULT_LINE_FORMAT = f"{vagranttoansible.DEFAULT_LINE_FORMAT} ansible_ssh_transfer_method=scp"
+# vagranttoansible.DEFAULT_LINE_FORMAT = "[{host}]\n" \
+#                                        "{host} " \
+#                                        "ansible_user={user} " \
+#                                        "ansible_host={ssh_hostname} " \
+#                                        "ansible_port={ssh_port} " \
+#                                        "ansible_private_key_file={private_file}"
+vagranttoansible.cli()
